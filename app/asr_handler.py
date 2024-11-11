24189dd29e0fc797c63a5d829e3129d2399a3a00
Ali Arefi <ali.are@mtnirancell.com>
2024-11-11 17:06:18 +0330

    add message brocker handler

--- app/asr_handler.py
+++ app/asr_handler.py
@@ -1,30 +1,23 @@
 import os
 from app.services.asr_service import ASRService
-from app.config import AUDIO_TEMP_PATH
-
-# Assuming your ASRService is already implemented
-asr_service = ASRService(model_path="app/models/vosk-model-en-us-0.22-lgraph")
+from app.config import AUDIO_TEMP_PATH, MODEL_PATH
+from app.send_task_queue import send_to_translation_queue
+asr_service = ASRService(model_path=MODEL_PATH)
 
 def process_audio(audio_data):
     """
     This function receives the audio data, saves it temporarily, and processes it with the ASR service.
     """
     try:
-        # Save the incoming audio data to a temporary file
-        file_path = os.path.join(AUDIO_TEMP_PATH, "audio.wav")  # Use a constant name since only one request at a time
+        file_path = os.path.join(AUDIO_TEMP_PATH, "audio.wav")
         
         with open(file_path, "wb") as audio_file:
             audio_file.write(audio_data)
 
         transcription = asr_service.transcribe_audio(file_path)
-        
-        # Send the transcription to a result queue or store it as needed
         print(f"Transcription: {transcription}")
-        print(f"asr_service.results after: {asr_service.results}")
+        transcription_text = asr_service.results.get('transcription')
+        send_to_translation_queue(transcription_text)
         
-        # Here you would send the transcription back to a translation service or return it
-        # For example, you could call a function to push the result to a translation queue or return the result.
-
     except Exception as e:
         print(f"Error processing audio: {e}")
-        # You can log this error or handle it as needed
--- app/queue_listener.py
+++ app/queue_listener.py
@@ -1,7 +1,27 @@
-
 import pika
+import json
 from app.asr_handler import process_audio
-from app.config import RABBITMQ_HOST, ASR_QUEUE
+from app.config import RABBITMQ_HOST, ASR_QUEUE, TRANSLATION_QUEUE
+
+def send_to_translation_queue(transcription_text):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
+    channel = connection.channel()
+
+    channel.queue_declare(queue=TRANSLATION_QUEUE)
+
+    message = {
+        "text": transcription_text  
+    }
+    
+    channel.basic_publish(
+        exchange="",
+        routing_key=TRANSLATION_QUEUE,
+        body=json.dumps(message) 
+    )
+
+    print("Sent transcription to translation queue:", transcription_text)
+
+    connection.close()
 
 def listen_for_audio_tasks():
     """
