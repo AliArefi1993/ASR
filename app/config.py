048e6366a212c5bc52e49da6d0920ac5840aff62
Ali Arefi <ali.are@mtnirancell.com>
2024-11-25 18:22:12 +0330

    remove temp directory

--- app/config.py
+++ app/config.py
@@ -8,5 +8,5 @@ ASR_QUEUE = "asr_task_queue"
 TRANSLATION_QUEUE = "translation_task_queue"
 RESPONSE_QUEUE = "response_queue"
 
-AUDIO_TEMP_PATH="app/temp/"
+AUDIO_TEMP_PATH="app/"
 MODEL_PATH="/app/app/models/vosk-model-en-us-0.22-lgraph"
